f6bf36fc398420aadd231eb6b4391c8e5f0a28ff
MarkHiarley <hiarleycostamarley@gmail.com>
2024-12-24 15:29:03 -0300

    melhorias, em arquivos e imagems com tailwind

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,7 +2,7 @@ const NavBar = () => {
     return(
         <div className="flex aling-center justify-between bg-[#F0B2A9] p-4">
             <ul className="flex flex-row gap-20 items-center justify-center w-full text-white">
-                <li><img src="sd" alt="LOGO" /></li>
+                
                 <li>Formação</li>
                 <li>Principais Serviços</li>
                 <li>Quem sou</li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,24 @@ export default function Home() {
       <section >
         <NavBar />
         <div>
-          <div className="flex flex-col items-center p-16" >
-            <img src="asa" alt="LOGO" className="p-4" />
-            <Button/>
+          <div className="flex flex-col items-center p-8" >
+            <img src="./img/logo.png" alt="LOGO" className="w-1/5  p-4 pb-7" />
+            <Button />
           </div>
+          <p className="text-center pb-3 text-2xl">Quem sou eu?</p>
           <div className="flex flex-row items-center justify-center w-full">
-            <div className="flex flex-row items-center justify-center w-2/6">
-              <p className="flex flex-wrap text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, corporis iste? Porro error rerum fugiat cum. Laboriosam vero alias blanditiis, consequatur eos praesentium dolorem consectetur dolor, deleniti corrupti, tempora molestiae.
+            <div className="flex flex-row items-center justify-center w-4/6 h-auto">
+              <p className="flex flex-wrap text-center text-xl pr-4 h-2/4">
+
+                Sou Edvânia, empreendedora, designer de sobrancelhas, designer de cílios e epiladora. Sou apaixonada pelo meu trabalho e mãe de três filhos maravilhosos.
+
+                Comecei minha trajetória na área da beleza há cerca de três anos, atuando inicialmente como manicure. Com o tempo, descobri minha paixão pelas sobrancelhas, que ganharam meu coração. Mais recentemente, me aventurei no mundo da epilação e dos cílios, e foi aí que tive a certeza de estar no lugar certo, fazendo exatamente o que amo.
+
+                Tenho um olhar atento aos detalhes e um senso apurado de design, sempre buscando superar as expectativas de minhas clientes.
+
+                Sou profundamente grata a cada cliente maravilhosa que me acompanha desde o início, me incentivando a crescer e buscar sempre o melhor.
               </p>
-              <img src="as" alt="FOTO DA MUIER" />
+              <img src="./img/PhotoMain.png" alt="FOTO DA MUIER" className="w-96 rounded-md " />
             </div>
           </div>
         </div>
