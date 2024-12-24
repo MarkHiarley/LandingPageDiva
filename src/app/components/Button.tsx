974b39e938f4a67a283d69940f07344d7432cdbe
MarkHiarley <hiarleycostamarley@gmail.com>
2024-12-24 14:10:03 -0300

    test upload

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,15 @@
+const Button = () =>{
+    return (
+        <button
+        className="
+        bg-[#F0B2A9]
+        text-white
+        rounded-full
+        p-2
+        shadow-md
+        w-40
+        ">AGENDE AGORA!</button>
+    )
+}
+
+export default Button;
---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return(
+        <div className="flex aling-center justify-between bg-[#F0B2A9] p-4">
+            <ul className="flex flex-row gap-20 items-center justify-center w-full text-white">
+                <li><img src="sd" alt="LOGO" /></li>
+                <li>Formação</li>
+                <li>Principais Serviços</li>
+                <li>Quem sou</li>
+            </ul>
+        </div>
+    )
+}
+export default NavBar;
