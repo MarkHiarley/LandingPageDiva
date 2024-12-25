3be5020944c6318374f284afb07aacd24e5f10fd
MarkHiarley <hiarleycostamarley@gmail.com>
2024-12-24 23:09:53 -0300

    responsividade page /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,45 @@ import NavBar from "./components/NavBar";
 export default function Home() {
   return (
     <>
-      <section >
+      <section>
         <NavBar />
-        <div>
-          <div className="flex flex-col items-center p-8" >
-            <img src="./img/logo.png" alt="LOGO" className="w-1/5  p-4 pb-7" />
+        <div className="flex flex-col items-center p-4 md:p-8">
+      
+          <div className="flex flex-col items-center">
+            <img 
+              src="./img/logo.png" 
+              alt="LOGO" 
+              className="w-1/2 md:w-1/5 p-4 pb-7" 
+            />
             <Button />
-          </div>
-          <p className="text-center pb-3 text-2xl">Quem sou eu?</p>
-          <div className="flex flex-row items-center justify-center w-full">
-            <div className="flex flex-row items-center justify-center w-4/6 h-auto">
-              <p className="flex flex-wrap text-center text-xl pr-4 h-2/4">
-
+          </div>     
+          <p className="text-center p-3 text-xl md:text-2xl font-semibold">
+            Quem sou eu?
+          </p>
+        
+          <div className="flex flex-col md:flex-row items-center justify-center w-full gap-6">
+      
+            <div className="flex flex-col items-center md:items-start text-justify w-full md:w-2/3 px-4">
+              <p className="text-base md:text-lg leading-relaxed">
                 Sou Edvânia, empreendedora, designer de sobrancelhas, designer de cílios e epiladora. Sou apaixonada pelo meu trabalho e mãe de três filhos maravilhosos.
-
+                <br />
+                <br />
                 Comecei minha trajetória na área da beleza há cerca de três anos, atuando inicialmente como manicure. Com o tempo, descobri minha paixão pelas sobrancelhas, que ganharam meu coração. Mais recentemente, me aventurei no mundo da epilação e dos cílios, e foi aí que tive a certeza de estar no lugar certo, fazendo exatamente o que amo.
-
+                <br />
+                <br />
                 Tenho um olhar atento aos detalhes e um senso apurado de design, sempre buscando superar as expectativas de minhas clientes.
-
+                <br />
+                <br />
                 Sou profundamente grata a cada cliente maravilhosa que me acompanha desde o início, me incentivando a crescer e buscar sempre o melhor.
               </p>
-              <img src="./img/PhotoMain.png" alt="FOTO DA MUIER" className="w-96 rounded-md " />
+            </div>
+         
+            <div className="w-full md:w-1/3 flex justify-center">
+              <img 
+                src="./img/PhotoMain.png" 
+                alt="FOTO DA MUIER" 
+                className="w-full md:w-96 rounded-md shadow-lg" 
+              />
             </div>
           </div>
         </div>
