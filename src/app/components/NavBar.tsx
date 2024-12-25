11ab5aa353c0f82de3da00a2683a4aefa7849b7c
MarkHiarley <hiarleycostamarley@gmail.com>
2024-12-25 13:45:10 -0300

    animacoes e tentativa de implementar formacoes

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,9 +3,12 @@ const NavBar = () => {
         <div className="flex aling-center justify-between bg-[#F0B2A9] p-4">
             <ul className="flex flex-row gap-20 items-center justify-center w-full text-white">
                 
-                <li>Formação</li>
-                <li>Principais Serviços</li>
-                <li>Quem sou</li>
+                <a href="" className="hover:scale-110 transition-transform duration-300"><li>Formação</li></a>
+                
+                <a href="" className="hover:scale-110 transition-transform duration-300"><li>Principais Serviços</li></a>
+                
+                <a href="" className="hover:scale-110 transition-transform duration-300"><li>Quem sou</li></a>
+
             </ul>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,18 @@ export default function Home() {
       <section>
         <NavBar />
         <div className="flex flex-col items-center p-4 md:p-8">
-      
           <div className="flex flex-col items-center">
-            <img 
-              src="./img/logo.png" 
-              alt="LOGO" 
-              className="w-1/2 md:w-1/5 p-4 pb-7" 
+            <img
+              src="./img/logo.png"
+              alt="LOGO"
+              className="w-1/2 md:w-1/5 p-4 pb-7"
             />
-            <Button />
-          </div>     
+            <a href="https://api.whatsapp.com/send/?phone=5588992734978&text&type=phone_number&app_absent=0" className="hover:scale-110 transition-transform duration-300"><Button /></a>
+          </div>
           <p className="text-center p-3 text-xl md:text-2xl font-semibold">
             Quem sou eu?
           </p>
-        
           <div className="flex flex-col md:flex-row items-center justify-center w-full gap-6">
-      
             <div className="flex flex-col items-center md:items-start text-justify w-full md:w-2/3 px-4">
               <p className="text-base md:text-lg leading-relaxed">
                 Sou Edvânia, empreendedora, designer de sobrancelhas, designer de cílios e epiladora. Sou apaixonada pelo meu trabalho e mãe de três filhos maravilhosos.
@@ -36,13 +33,23 @@ export default function Home() {
                 Sou profundamente grata a cada cliente maravilhosa que me acompanha desde o início, me incentivando a crescer e buscar sempre o melhor.
               </p>
             </div>
-         
+
             <div className="w-full md:w-1/3 flex justify-center">
-              <img 
-                src="./img/PhotoMain.png" 
-                alt="FOTO DA MUIER" 
-                className="w-full md:w-96 rounded-md shadow-lg" 
+              <img
+                src="./img/PhotoMain.png"
+                alt="FOTO DA MUIER"
+                className="w-full md:w-96 rounded-md shadow-lg"
               />
+            </div>
+          </div>
+
+          <p className="text-center p-3 text-xl md:text-2xl font-semibold">
+            Formação
+          </p>
+
+          <div>
+            <div className="flex flex-col items-center md:items-start text-justify w-full md:w-2/3 px-4">
+              
             </div>
           </div>
         </div>
